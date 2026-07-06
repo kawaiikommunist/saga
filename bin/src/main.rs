44c3dc700d0976dc8a360c9d53f27cbbf1bca690
linde <lindenmixon@gmail.com>
2026-07-06 18:06:44 -0500

    ok2
    Signed-off-by: linde <lindenmixon@gmail.com>

--- bin/src/main.rs
+++ bin/src/main.rs
@@ -1,4 +1,11 @@
-use bevy::prelude::*;
+pub mod cut {
+    pub use bevy::prelude::*;
+}
+
+mod app;
+mod game;
+
+use cut::*;
 
 fn main() -> AppExit {
     App::new().add_plugins(DefaultPlugins).run()
